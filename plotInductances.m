f5ac1e9d22b0a3b5a660fdb89abd50ac60329f45
rohan-inston <reggiedavies26@gmail.com>
2020-02-27 16:46:32 +0000

    comments and clean up
    as title

--- plotInductances.m
+++ plotInductances.m
@@ -1,13 +1,12 @@
-load('psi.mat');
+%load('psi.mat');
 
-psitemp = zeros(1,10)
+psitemp = zeros(1,10);
 hold on
-for i = 1:6
-    for j = 1:10
+for i = 1:4
+    for j = 10:1:30
       psitemp(j) = psi(i,2,j);
-       
     end
-    plot(1:1:10, psitemp)
+    plot(1:30, psitemp)
     
 end
 hold off
